a1a5cfebcf105a859a81340309877bb67a1ed8e4
bmbp <zc_union@163.com>
2024-09-03 23:24:34 +0800

    dict

--- crates/bmbp_config/web/tsx/dict/form/form.tsx
+++ crates/bmbp_config/web/tsx/dict/form/form.tsx
@@ -1,11 +1,33 @@
 export const DictAddForm = () => {
-    return (<arco.Form>
-            <arco.Form.Item label='字典名称' name='dictName' rules={[{required: true, message: '请输入字典名称'}]}>
-                <arco.Input placeholder='请输入字典名称'/>
-            </arco.Form.Item>
-            <arco.Form.Item label='字典编码' name='dictCode' rules={[{required: true, message: '请输入字典编码'}]}>
-                <arco.Input placeholder='请输入字典编码'/>
-            </arco.Form.Item>
-        </arco.Form>
-    )
-}
+  return (
+    <arco.Form>
+      <arco.Form.Item label="上级字典编码" name="dictParentCode" hidden={true}>
+        <arco.Input placeholder="" />
+      </arco.Form.Item>
+      <arco.Form.Item
+        label="字典名称"
+        name="dictName"
+        rules={[{ required: true, message: "请输入字典名称" }]}
+      >
+        <arco.Input placeholder="请输入字典名称" />
+      </arco.Form.Item>
+      <arco.Form.Item
+        label="字典别名"
+        name="dictAlias"
+        rules={[{ required: true, message: "请输入字典别名" }]}
+      >
+        <arco.Input placeholder="请输入字典别名" />
+      </arco.Form.Item>
+      <arco.Form.Item
+        label="字典值"
+        name="dictValue"
+        rules={[{ required: true, message: "请输入字典值" }]}
+      >
+        <arco.Input placeholder="请输入字典值" />
+      </arco.Form.Item>
+      <arco.Form.Item label="字典显示顺序" name="dataSort">
+        <arco.Input.Number placeholder="请输入字典显示顺序" />
+      </arco.Form.Item>
+    </arco.Form>
+  );
+};
